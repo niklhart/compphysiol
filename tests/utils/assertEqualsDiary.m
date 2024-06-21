6e922e98344a8ff8ec86c051b4fbf833adc795e8
Niklas Hartung <niklas.hartung@uni-potsdam.de>
2024-06-21 09:19:26 +0200

    assertEqualsDiary continued 4

--- tests/utils/assertEqualsDiary.m
+++ tests/utils/assertEqualsDiary.m
@@ -12,18 +12,18 @@ function assertEqualsDiary(fh, refDiary)
     output_ref = fileread(refFile);
     
     % remove line breaks / tabs, which might differ between systems:
-    output_test = strrep(output_test, newline,' ');
-    output_ref  = strrep(output_ref, newline,' ');
+    output_test = strrep(output_test, newline, ' ');
+    output_ref  = strrep(output_ref, newline, ' ');
 
-    % merge repeated spaces
-    output_test = regexprep(output_test,' +',' ');
-    output_ref  = regexprep(output_ref,' +',' ');
+    % merge repeated spaces, remove leading/trailing spaces
+    output_test = strtrim(regexprep(output_test, ' +', ' '));
+    output_ref  = strtrim(regexprep(output_ref, ' +', ' '));
 
     % compare text to reference
     success = isequal(output_test, output_ref);
 
     if ~success
-        msg = 'Expected console output:\n%s\n\n Actual console output\n %s\n';
+        msg = 'Expected console output:\n%s\n\nActual console output:\n%s\n';
         error(msg, output_ref, output_test)
     end
 
